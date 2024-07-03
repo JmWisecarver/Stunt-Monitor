2399352cb947c1b7fc2b85dd9d584364c39a2745
JmWisecarver <35155170+JmWisecarver@users.noreply.github.com>
2024-07-02 19:40:06 -0700

    Updated to use HTTPS instead of HTTP
    Secure connection using certificate established. HTTPclient no longer used.

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 //              Kevin Wing,
 //              Gary Banks
 /********************STUNT MONITOR********************/
-//Kevin Wing also credited for formatting our paper in IEEE format
 
 #include <SoftwareSerial.h>
 #include "TinyGPS++.h"
@@ -16,12 +15,48 @@
 #include <ArduinoJson.h>
 #include "sdStunt.h"
 #include <Adafruit_NeoPixel.h>
+#include <WiFiClientSecure.h>
+
 
 #define ARDUINOJSON_USE_DOUBLE 1
 #define CS_PIN 15  // Chip select pin for the SD card, this can be any GPIO pin
 
 // MPU6050 addresses
 // #define MPU6050_ADDRESS_AD0_LOW 0x68 // address pin low (GND), default for InvenSense evaluation board
+
+const char * certificate = \
+"-----BEGIN CERTIFICATE-----\n" \
+"MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw\n" \
+"TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n" \
+"cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4\n" \
+"WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu\n" \
+"ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY\n" \
+"MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc\n" \
+"h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+\n" \
+"0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U\n" \
+"A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW\n" \
+"T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH\n" \
+"B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC\n" \
+"B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv\n" \
+"KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn\n" \
+"OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn\n" \
+"jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw\n" \
+"qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI\n" \
+"rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV\n" \
+"HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq\n" \
+"hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL\n" \
+"ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ\n" \
+"3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK\n" \
+"NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5\n" \
+"ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur\n" \
+"TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC\n" \
+"jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc\n" \
+"oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq\n" \
+"4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA\n" \
+"mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d\n" \
+"emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=\n" \
+"-----END CERTIFICATE-----\n";
+
 
 // Mutexes and Semaphores
 SemaphoreHandle_t i2cMutex;
@@ -34,7 +69,7 @@ double getRelativeDegrees(double base);
 bool uploadSDDataToServer(const char* filename, const String& data);
 void checkForPendingSD();
 
-// NOTE: a squid is a slang term for a "show off" on a street bike
+//Jeremy Wisecarver
 class squid
 {
 public:
@@ -67,7 +102,7 @@ public:
         stuntTime = 0;
     }
 };
-
+//Jeremy Wisecarver
 class dataQueues
 {
 public:
@@ -75,7 +110,7 @@ public:
     QueueHandle_t accelReadings;
     QueueHandle_t riderData;
 };
-
+//Kevin Wing
 typedef struct AccelData
 {
     double AccelX;
@@ -115,7 +150,7 @@ void setup()
     Adafruit_NeoPixel strip =
     Adafruit_NeoPixel(LED_LENGTH, NEOPIXEL_LED, NEO_GRBW + NEO_KHZ800);
     //Changing the GPS pins caused pixel to turn on so this just turns it back off in a quick and dirty fashion
-    strip.setPixelColor(0, 0x000000);
+        strip.setPixelColor(0, 0x000000);
         strip.setPixelColor(1, 0x000000);
         strip.setPixelColor(2, 0x000000);
         strip.setPixelColor(3, 0x000000);
@@ -158,9 +193,12 @@ void communicateWithServer_t(void * queue)
     QueueHandle_t riderDataQueue = (QueueHandle_t)queue;
     unsigned long lastTime = 0;
     unsigned long timerDelay = 5000;
-    const char * ssid = "JSW-Pixel";
-    const char* password = "2086403284";
-    String serverName = "http://stunt-monitor.lithium720.pw/receive.php";
+    const char * ssid = "Jerdroid1";
+    const char* password = "nulluswhalus";
+    const char*  serverName = "stunt-monitor.lithium720.pw";
+    const char* pathName = "https://stunt-monitor.lithium720.pw/receive.php";
+    String url = "/receive.php";
+    String host = "stunt-monitor.lithium720.pw";
     //Connect to phones hotspot
     WiFi.begin(ssid, password);
     Serial.println("Connecting");
@@ -210,20 +248,22 @@ void communicateWithServer_t(void * queue)
                         wasDisconnected = false;
                     }
 
-                    WiFiClient client;
-                    HTTPClient http;
-
-                    http.begin(client, serverName);
-                    int httpResponseCode = http.POST(jsonDoc);
-
-                    http.addHeader("Content-Type", "application/json");
-
-                    Serial.print("HTTP Response code: ");
-                    Serial.println(httpResponseCode);
-                    String payload = http.getString();
-                        Serial.println(payload);
-
-                    http.end();
+                    WiFiClientSecure client;
+                    client.setCACert(certificate);
+                    //client.setInsecure();
+                    if (!client.connect(serverName, 443))
+                        Serial.println("Connection failed!");
+                    else    {
+                        Serial.println("connected\n");
+                        client.print(String("POST ") + url + " HTTP/1.1\r\n" +
+                        "Host: " + host + "\r\n" +
+                        "Connection: close\r\n" +
+                        "Content-Type: application/json\r\n" +
+                        "Content-Length: " + jsonDoc.length() + "\r\n" +
+                        "\r\n" +
+                        jsonDoc + "\n");
+                    }
+               
                 }
                 else{
                         Serial.println("WiFi Disconnected, saving data to SD card.");
@@ -440,8 +480,6 @@ void readGPS_t(void *queue)
                 {
                     latlng[0] = gps.location.lat();
                     latlng[1] = gps.location.lng();
-                    //Serial.print("CurrentPos: ");
-                    //Serial.println(latlng[0]);
                     xQueueSend(gpsReadings, &latlng, 0);
                 }
             }
